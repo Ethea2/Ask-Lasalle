ba0b4484c1f32ea07c29f4cb1394b8d36f4e52ee
Nathan Andres <nathandres2001@gmail.com>
2023-06-13 02:59:17 +0800

    added links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Onboarding from "./pages/Onboarding";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
 import Homepage from "./pages/Homepage";
 
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Onboarding />} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/homepage' element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+export const Signup = () => {
+    return (
+        <div>
+            SIGNUP PAGE
+        </div>
+    )
+}
+
+export default Signup;
