866f0392b4509f85aa0a2fc186a938680719db3f
Nathan Andres <nathandres2001@gmail.com>
2023-06-16 20:35:30 +0800

    forgot

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,14 @@
 import Navbar from "../components/Navbar";
+import Postcard from "../components/Postcard";
 import Sidebar from "../components/Sidebar";
 
 const Homepage = () => {
+
+    const post = {
+        postid: 1,
+        title: "Where can I find agno?",
+        comments: 30
+    }
     return ( 
 
         <div className="homepage">
@@ -9,10 +16,14 @@ const Homepage = () => {
                 <Navbar />
             </div>
 
+            
             <div className="side" class="mt-14 ml-14 mb-14">
                 <Sidebar />
             </div>
             
+            <div>
+                <Postcard post={post}></Postcard>
+            </div>
             
         </div>
      );
